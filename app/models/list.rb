09cfd0ae5af7af8c659ed2d918badda6cf6dc1d2
Daniel <dk377@hotmail.com>
2018-11-29 21:55:24 -0200

    bootstrap finalizado

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ActiveRecord::Base
   
   belongs_to :user
-
+  validates_presence_of :name, :description
+  
   has_many :products, dependent: :destroy
 accepts_nested_attributes_for :products, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 
     has_many :lists
     validates_presence_of :name
+    validates_uniqueness_of :name
+    validates_numericality_of :age, greater_than_or_equal_to: 0
 end
